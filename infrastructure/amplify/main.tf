5130265de6d032d11040b389d8d5363841b76e80
Abdala <clahimoha1000@gmail.com>
2025-06-27 19:26:50 +0300

    installing amplify

---
+++ infrastructure/amplify/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.5"
+    }
+  }
+  required_version = ">= 1.7"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_amplify_app" "serverless_app" {
+  name       = "serverless-app"
+  repository = "https://github.com/clahi/A-Serverless-Web-Application-using-Generative-AI"
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - cd ai-recipe-generator/
+            - ls
+            - npm install
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # GitHub personal access token
+  access_token = var.access_token
+
+  enable_auto_branch_creation = true
+
+  # The default patterns added by the Amplify Console.
+  auto_branch_creation_patterns = [
+    "*",
+    "*/**",
+  ]
+
+  auto_branch_creation_config {
+    # Enable auto build for the created branch.
+    enable_auto_build = true
+  }
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+  
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.serverless_app.id
+  branch_name = "main"
+
+  enable_auto_build = true
+
+  stage = "PRODUCTION"
+
+  environment_variables = {
+    "ENV" = "production"
+  }
+}
---
+++ infrastructure/amplify/variables.tf
@@ -0,0 +1,5 @@
+variable "access_token" {
+  description = "The access token to git repository"
+  sensitive   = true
+  type        = string
+}
